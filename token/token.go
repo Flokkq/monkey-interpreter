d6f8a1dbd1fd05cd471dac1be3241d7ea76290f5
Flokkq <webcla21@htl-kaindorf.at>
2024-09-22 17:06:32 +0200

    feat(token): define token literals

---
+++ token/token.go
@@ -0,0 +1,30 @@
+package token
+
+type TokenTypte string
+
+type Token struct {
+	Type    TokenTypte
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	IDENT = "IDENT"
+	INT   = "INT"
+
+	ASSIGN = "="
+	PLUS   = "+"
+
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
