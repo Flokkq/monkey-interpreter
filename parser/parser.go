36d6da5b9de76c42f71b93c6087096cc3aedc849
Flokkq <webcla21@htl-kaindorf.at>
2024-09-25 22:50:11 +0200

    feat(parser): add error handling if there is no parser for an expression

--- parser/parser.go
+++ parser/parser.go
@@ -168,12 +168,18 @@ func (p *Parser) parseExpression(precedence int) ast.Expression {
 	prefix := p.prefixParseFns[p.currentToken.Type]
 
 	if prefix == nil {
+		p.noPrefixParseFnError(p.currentToken.Type)
 		return nil
 	}
 
 	leftExp := prefix()
 
 	return leftExp
+}
+
+func (p *Parser) noPrefixParseFnError(t token.TokenType) {
+	msg := fmt.Sprintf("no prefix parse function for %s found", t)
+	p.errors = append(p.errors, msg)
 }
 
 func (p *Parser) parseIdentifier() ast.Expression {
