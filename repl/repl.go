ca0e6614a979c4ae6b405f4eb71596132bdcce9e
Flokkq <webcla21@htl-kaindorf.at>
2024-10-01 21:00:22 +0200

    feat(repl): more monkey

--- repl/repl.go
+++ repl/repl.go
@@ -10,6 +10,19 @@ import (
 )
 
 const PROMPT = ">> "
+const MONKEY_FACE = `
+       __,__
+      .--.  .-"     "-.  .--.
+     / .. \/  .-. .-.  \/ .. \
+    | |  '|  /   Y   \  |'  | |
+    | \   \  \ 0 | 0 /  /   / |
+     \ '- ,\.-""""""""-./, -' /
+      '._/  _     :     _  \_.'
+         |  \    :    /  |
+         \   '.  :  .'   /
+          '._   '--'   _.'
+             '-._____.-'
+    `
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
@@ -38,6 +51,10 @@ func Start(in io.Reader, out io.Writer) {
 }
 
 func printParseErrors(out io.Writer, errors []string) {
+	io.WriteString(out, MONKEY_FACE)
+	io.WriteString(out, "Woops! We ran into some monkey business here!\n")
+	io.WriteString(out, " parser errors:\n")
+
 	for _, msg := range errors {
 		io.WriteString(out, "\t"+msg+"\n")
 	}
